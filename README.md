837791845cc4daaa305d12597259145da69f8756
Martin Jönsson <mr_tines_j@hotmail.com>
2022-06-22 15:37:33 +0200

    cleared project

--- README.md
+++
@@ -1,2 +0,0 @@
-# DjangoReact
-demo
--- backend/products/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from .serializers import ProductSerializer
-from .models import Product
-
-class ProductView(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
